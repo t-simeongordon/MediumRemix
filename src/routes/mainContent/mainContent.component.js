47342f34c1e64d0a06328c6365d988f3575d0d84
t s g <tsg@tsg.local>
2022-08-26 10:47:17 +0100

    feat-1: axios added

--- src/routes/mainContent/mainContent.component.js
+++ src/routes/mainContent/mainContent.component.js
@@ -1,9 +1,36 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import axios from "axios"
+
+const url = "https://dqzzzp9cuf.execute-api.eu-west-2.amazonaws.com/Prod/ehealth/records?type=medicine"
+const headers = {
+    headers: { 
+        'content-type': 'application/json', 
+        'Access-Control-Allow-Origin':'*',
+        "Access-Control-Allow-Headers": "X-Requested-With" 
+    }
+} 
+
+const params ={
+    type : "medicine"
+}
 
 const MainContent = () => {
+    const [content, setContent] = useState("")
+
+    const getContent = async() =>{
+        const topicContent = await axios.get(url, params, headers)
+        console.log(`topicContent::: ${JSON.stringify(topicContent.data)}`)
+        console.log(`content 1::: ${JSON.stringify(content)}`)
+        setContent("S3 GET")
+        console.log(`content 2::: ${JSON.stringify(content)}`)
+    }
+
     return (
         <>
             <div>Main Content</div>
+            <p>{content}</p>
+            <button onClick={getContent}>click</button>
             <Outlet/>
         </>
     ) 
